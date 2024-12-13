2954718ec815f9f40f9c6303a9081735eaf52fc5
reinatt <reinatthanselwijaya@gmail.com>
2024-12-13 23:04:56 +0100

    last please

--- 394199/tm.cpp
+++ 394199/tm.cpp
@@ -1,309 +1,297 @@
-#include "macros.h"
-#include <atomic>
-#include <chrono>
+/**
+ * @file   tm.c
+ * @author [...]
+ *
+ * @section LICENSE
+ *
+ * [...]
+ *
+ * @section DESCRIPTION
+ *
+ * Implementation of your own transaction manager.
+ * You can completely rewrite this file (and create more files) as you wish.
+ * Only the interface (i.e. exported symbols and semantic) must be preserved.
+**/
+
+// Requested features
+#define _GNU_SOURCE
+#define _POSIX_C_SOURCE   200809L
+#ifdef __STDC_NO_ATOMICS__
+    #error Current C11 compiler does not support atomic operations
+#endif
+
+// External headers
+
+// Internal headers
+#include "tm.hpp"
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <atomic>
+#include <unordered_set>
 #include <map>
 #include <memory>
 #include <shared_mutex>
-#include <string.h>
-#include <tm.hpp>
-#include <unordered_set>
 #include <vector>
 #include "lock.hpp"
 
-struct VersionLockValue {
-  bool locked;
-  uint64_t version;
-  uint64_t lock; // locked | version (concated)
+#include "macros.h"
+
+using namespace std;
+
+/**
+ * @brief Simple Shared Memory Region (a.k.a Transactional Memory).
+ */
+
+struct Segment{
+    lock_t lock = lock_t();
+    uint64_t data = 0;
 };
 
-class VersionLock {
-private:
-  std::atomic_uint64_t vlock;
-
-public:
-  VersionLock() : vlock(0) {}
-  VersionLock(const VersionLock &vl) { vlock = vl.vlock.load(); }
-  
-  bool TryAcquire() {
-    VersionLockValue val = this->Sample();
-    if (val.locked) {
-      return false;
-    }
-
-    return this->TryCompareAndSwap(true, val.version, val.lock);
-  }
-
-  // releases lock
-  bool Release() {
-    VersionLockValue val = this->Sample();
-    if (!val.locked) {
-      printf("[VersionLock\tRelease]: releasing unlocked lock\n");
-      return false;
-    }
-
-    return this->TryCompareAndSwap(false, val.version, val.lock);
-  }
-  
-  // atomicaly sets lock version and releases lock
-  bool VersionedRelease(uint64_t new_version) {
-    VersionLockValue val = this->Sample();
-    if (!val.locked) {
-      printf("[VersionLock\tVersionedRelease]: releasing unlocked lock\n");
-      return false;
-    }
-
-    return this->TryCompareAndSwap(false, new_version, val.lock);
-  }
-
-  // atomicaly samples lock and returns {lock bit, version} as VersionLockValue
-  VersionLockValue Sample() {
-    uint64_t current = vlock.load();
-    return Parse(current);
-  }
-
-  // return true if CAS succeeds, false otherwise
-  bool TryCompareAndSwap(bool do_lock, uint64_t desired_version,
-                         uint64_t compare_to) {
-    uint64_t new_lock = Serialize(do_lock, desired_version);
-    return this->vlock.compare_exchange_strong(compare_to, new_lock);
-  }
-
-  // concats lock bit and version into a uint64
-  uint64_t Serialize(bool locked, uint64_t version) {
-    if ((version >> 63) == 1) {
-      printf("[VersionLock\tSerialize]: version overflow\n");
-      throw -1;
-    }
-
-    if (locked) {
-      return ((uint64_t)1 << 63) | version;
-    }
-    return version;
-  }
-  
-  // returns {lock bit, version} as VersionLockValue of given uint64
-  VersionLockValue Parse(uint64_t serialized) {
-    uint64_t version = (((uint64_t)1 << 63) - 1) & serialized;
-    uint64_t locked_bit = serialized >> 63;
-    return {locked_bit == 1, version, serialized};
-  }
-};
-
-static std::atomic_uint global_vc = 0; // global version clock
-
-struct target_src {
-  uintptr_t target;
-  void *src;
+struct Region {
+    Region(size_t size, size_t align): 
+        size(size), align(align), locks(500, vector<Segment>(1500)){}
+    size_t size;        // Size of the non-deallocable memory segment (in bytes)
+    size_t align;       // Size of a word in the shared memory region (in bytes)
+    atomic<uint64_t> global_version{0}; // Global version number
+    atomic<uint64_t> segment_count{2}; // Global segment count
+    vector<vector<Segment>> locks; //Lock for each segment
 };
 
 struct Transaction {
-  std::unordered_set<void *> read_set;   // set of read words
-  std::map<uintptr_t, void *> write_set; // target word -> src word
-  uint64_t rv;                           // read-version
-  uint64_t wv;                           // write-version
-  bool read_only = false;
+    unordered_set<void*> read_set;
+    map<uintptr_t, void*> write_set;
+    uint64_t rv;
+    bool is_ro;
 };
 
-static thread_local Transaction transaction;
+static thread_local shared_ptr<Transaction> the_transaction;
 
-struct WordLock {
-  lock_t vlock;
-  uint64_t word = 0;
-};
-
-struct region { // shared-mem
-  region(size_t size, size_t align)
-      : size(size), align(align), mem(500, std::vector<WordLock>(1500)) {}
-  size_t size;  // Size of the non-deallocable memory segment (in bytes)
-  size_t align; // Size of a word in the shared memory region (in bytes)
-  std::atomic_uint64_t seg_cnt = 2;
-  std::vector<std::vector<WordLock>> mem;
-};
-
-WordLock &getWordLock(struct region *reg, uintptr_t addr) {
-  return reg->mem[addr >> 32][((addr << 32) >> 32) / reg->align];
+Segment& get_segment(Region* region, uintptr_t addr){
+    return region->locks[addr >> 48][((addr << 16) >> 16)/region->align];
 }
 
-void reset_transaction() {
-  transaction.rv = 0;
-  transaction.read_only = false;
-  for (const auto &ptr : transaction.write_set) {
-    free(ptr.second);
-  }
-  transaction.write_set.clear();
-  transaction.read_set.clear();
+void abort_transaction(){
+    the_transaction->rv = 0;
+    the_transaction->is_ro = false;
+    for (const auto &entry : the_transaction->write_set) {
+        free(entry.second);
+    }
+    the_transaction->write_set.clear();
+    the_transaction->read_set.clear();
 }
 
-shared_t tm_create(size_t size, size_t align) noexcept {
-  region *region = new struct region(size, align);
-  if (unlikely(!region))
-    return invalid_shared;
-  return region;
+/** Create (i.e. allocate + init) a new shared memory region, with one first non-free-able allocated segment of the requested size and alignment.
+ * @param size  Size of the first shared segment of memory to allocate (in bytes), must be a positive multiple of the alignment
+ * @param align Alignment (in bytes, must be a power of 2) that the shared memory region must support
+ * @return Opaque shared memory region handle, 'invalid_shared' on failure
+**/
+shared_t tm_create(size_t size, size_t align) noexcept{
+    Region* region = new Region(size, align);
+    if (unlikely(!region)) {
+        return invalid_shared;
+    }
+    return region;
 }
 
-void tm_destroy(shared_t shared) noexcept {
-  struct region *reg = (struct region *)shared;
-  delete reg;
+/** Destroy (i.e. clean-up + free) a given shared memory region.
+ * @param shared Shared memory region to destroy, with no running transaction
+**/
+void tm_destroy(shared_t shared) noexcept{
+    // cout << "destroying" << endl;
+    Region* region = (struct Region*) shared;
+    delete region;
 }
 
-void *tm_start(shared_t unused(shared)) noexcept {
-  return (void *)((uint64_t)1 << 32);
+/** [thread-safe] Return the start address of the first allocated segment in the shared memory region.
+ * @param shared Shared memory region to query
+ * @return Start address of the first allocated segment
+**/
+void* tm_start(shared_t unused(shared)) noexcept{
+    // cout << "starting" << endl;
+    return (void *)((uint64_t)1 << 48);
 }
 
-size_t tm_size(shared_t shared) noexcept {
-  return ((struct region *)shared)->size;
+/** [thread-safe] Return the size (in bytes) of the first allocated segment of the shared memory region.
+ * @param shared Shared memory region to query
+ * @return First allocated segment size
+**/
+size_t tm_size(shared_t shared) noexcept{
+    // cout << "sizing" << endl;
+    return ((struct Region*) shared)->size;
 }
 
-size_t tm_align(shared_t shared) noexcept {
-  return ((struct region *)shared)->align;
+/** [thread-safe] Return the alignment (in bytes) of the memory accesses on the given shared memory region.
+ * @param shared Shared memory region to query
+ * @return Alignment used globally
+**/
+size_t tm_align(shared_t shared) noexcept{
+    // cout << "aligning" << endl;
+    return ((struct Region*) shared)->align;
 }
 
-tx_t tm_begin(shared_t unused(shared), bool is_ro) noexcept {
-  transaction.rv = global_vc.load();
-  transaction.read_only = is_ro;
-  return (uintptr_t)&transaction;
+/** [thread-safe] Begin a new transaction on the given shared memory region.
+ * @param shared Shared memory region to start a transaction on
+ * @param is_ro  Whether the transaction is read-only
+ * @return Opaque transaction ID, 'invalid_tx' on failure
+**/
+tx_t tm_begin(shared_t shared, bool is_ro) noexcept{
+    // cout << the_transaction << endl;
+    if(!the_transaction)
+        the_transaction = make_shared<Transaction>();
+    the_transaction->is_ro = is_ro;
+    the_transaction->rv = ((struct Region*) shared)->global_version.load();
+    // the_transaction->read_set.clear();
+    // the_transaction->write_set.clear();
+    return reinterpret_cast<tx_t>(the_transaction.get());
 }
 
-bool tm_write(shared_t shared, tx_t unused(tx), void const *source, size_t size,
-              void *target) noexcept {
+/** [thread-safe] End the given transaction.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to end
+ * @return Whether the whole transaction committed
+**/
+bool tm_end(shared_t shared, tx_t unused(tx)) noexcept{
+    if(the_transaction->is_ro || the_transaction->write_set.empty()){
+        abort_transaction();
+        return true;
+    }
+    Region* region = (struct Region*) shared;
 
-  struct region *reg = (struct region *)shared;
-
-  for (size_t i = 0; i < size / reg->align; i++) {
-    uintptr_t target_word = (uintptr_t)target + reg->align * i;    // shared
-    void *src_word = (void *)((uintptr_t)source + reg->align * i); // private
-    void *src_copy = malloc(reg->align); // be sure to free this
-    memcpy(src_copy, src_word, reg->align);
-    transaction.write_set[target_word] = src_copy; // target->src
-  }
-
-  return true;
-}
-
-bool tm_read(shared_t shared, tx_t unused(tx), void const *source, size_t size,
-             void *target) noexcept {
-  struct region *reg = (struct region *)shared;
-
-  // for each word
-  for (size_t i = 0; i < size / reg->align; i++) {
-    uintptr_t word_addr = (uintptr_t)source + reg->align * i;
-    WordLock &word = getWordLock(reg, word_addr);                     // shared
-    void *target_word = (void *)((uintptr_t)target + reg->align * i); // private
-
-    if (!transaction.read_only) {
-      auto it = transaction.write_set.find(word_addr); // O(logn)
-      if (it != transaction.write_set.end()) {         // found in write-set
-        memcpy(target_word, it->second, reg->align);
-        continue;
-      }
+    for (auto it = the_transaction->write_set.begin(); it != the_transaction->write_set.end(); ++it){
+        Segment &segment = get_segment(region, it->first);
+        if(!lock_acquire(&segment.lock)){
+            for(auto temp_it = the_transaction->write_set.begin(); temp_it != it; ++temp_it){
+                Segment &temp_segment = get_segment(region, temp_it->first);
+                lock_release(&temp_segment.lock);
+            }
+            abort_transaction();
+            return false;
+        }
     }
 
-    uint64_t prev_val = get_lock(&word.vlock);
-    memcpy(target_word, &word.word, reg->align); // read word
-    uint64_t post_val = get_lock(&word.vlock);
+    uint64_t wv = region->global_version.fetch_add(1)+1;
 
-    if ((post_val & 1) || ((prev_val >> 1) != (post_val >> 1)) ||
-        ((prev_val >> 1) > transaction.rv)) {
-      reset_transaction();
-      return false;
+    if(the_transaction->rv + 1 != wv){
+        for(const auto& entry: the_transaction->read_set){
+            Segment &segment = get_segment(region, reinterpret_cast<uintptr_t>(entry));
+            uint64_t val = get_lock(&segment.lock);
+            if ((val & 1) || (val >> 1) > the_transaction->rv){
+                for(const auto& entry : the_transaction->write_set){
+                    Segment &temp_segment = get_segment(region, entry.first);
+                    lock_release(&temp_segment.lock);
+                }
+                abort_transaction();
+                return false;
+            }
+        }
     }
-
-    if (!transaction.read_only)
-      transaction.read_set.emplace((void *)word_addr);
-  }
-
-  return true;
-}
-
-void release_lock_set(region *reg, uint i) {
-  if (i == 0)
-    return;
-  for (const auto &target_src : transaction.write_set) {
-    WordLock &wl = getWordLock(reg, target_src.first);
-    lock_release(&wl.vlock);
-    if (i <= 1)
-      break;
-    i--;
-  }
-}
-
-int try_acquire_sets(region *reg, uint *i) {
-  *i = 0;
-  for (const auto &target_src : transaction.write_set) {
-    WordLock &wl = getWordLock(reg, target_src.first);
-    bool acquired = lock_acquire(&wl.vlock);
-    if (!acquired) {
-      release_lock_set(reg, *i);
-      return false;
+    for(const auto& entry : the_transaction->write_set){
+        Segment &segment = get_segment(region, entry.first);
+        memcpy(&segment.data, entry.second, region->align);
+        if(!lock_release(&segment.lock, wv)){
+            abort_transaction();
+            return false;
+        }
     }
-    *i = *i + 1;
-  }
-  return true;
-}
-
-bool validate_readset(region *reg) {
-  for (const auto word : transaction.read_set) {
-    WordLock &wl = getWordLock(reg, (uintptr_t)word);
-    uint64_t val = get_lock(&wl.vlock);
-    if ((val & 1) || (val>>1) > transaction.rv) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-// release locks and update their version
-bool commit(region *reg) {
-
-  for (const auto target_src : transaction.write_set) {
-    WordLock &wl = getWordLock(reg, target_src.first);
-    memcpy(&wl.word, target_src.second, reg->align);
-    if (!lock_release(&wl.vlock, transaction.wv)) {
-      printf("[Commit]: VersionedRelease failed\n");
-      reset_transaction();
-      return false;
-    }
-  }
-
-  reset_transaction();
-  return true;
-}
-
-bool tm_end(shared_t unused(shared), tx_t unused(tx)) noexcept {
-  if (transaction.read_only || transaction.write_set.empty()) {
-    reset_transaction();
+    abort_transaction();
     return true;
-  }
-
-  struct region *reg = (struct region *)shared;
-
-  uint tmp;
-  if (!try_acquire_sets(reg, &tmp)) {
-    reset_transaction();
-    return false;
-  }
-
-  transaction.wv = global_vc.fetch_add(1) + 1;
-
-  if ((transaction.rv != transaction.wv - 1) && !validate_readset(reg)) {
-    release_lock_set(reg, tmp);
-    reset_transaction();
-    return false;
-  }
-
-  return commit(reg);
 }
 
-Alloc tm_alloc(shared_t shared, tx_t unused(tx), size_t unused(size),
-               void **target) noexcept {
-  struct region *reg = ((struct region *)shared);
-  *target = (void *)(reg->seg_cnt.fetch_add(1) << 32);
-  return Alloc::success;
+/** [thread-safe] Read operation in the given transaction, source in the shared region and target in a private region.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to use
+ * @param source Source start address (in the shared region)
+ * @param size   Length to copy (in bytes), must be a positive multiple of the alignment
+ * @param target Target start address (in a private region)
+ * @return Whether the whole transaction can continue
+**/
+bool tm_read(shared_t shared, tx_t unused(tx), void const* source, size_t size, void* target) noexcept{
+    Region* region = (struct Region*) shared;
+
+    for(size_t i = 0; i < size / region->align; i++){
+        uintptr_t addr = ((uintptr_t)source + region->align * i);
+        Segment &segment = get_segment(region, addr);
+        uint64_t prev_value = get_lock(&segment.lock);
+        void* the_target = (void*)((uintptr_t)target + region->align * i); 
+
+        if ((prev_value & 1) || (prev_value >> 1) > the_transaction->rv){
+            // handle abort logic -> nothing
+            abort_transaction();
+            return false;
+        }
+
+        if (the_transaction->is_ro) {
+            memcpy(the_target, &segment.data, size);
+            uint64_t post_value = get_lock(&segment.lock);
+            if ((post_value & 1) || (post_value >> 1) > the_transaction->rv || (post_value >> 1) != (prev_value >> 1)){
+                // handle abort logic -> nothing
+                abort_transaction();
+                return false;
+            }
+            continue;
+        }
+        //if not read only
+        else{
+            auto it = the_transaction->write_set.find(addr);
+            if (it != the_transaction->write_set.end()){
+                memcpy(target, it->second, region->align);
+                continue;
+            }
+            the_transaction->read_set.emplace(reinterpret_cast<void*>(addr));
+            memcpy(target, &segment.data, size);
+
+            uint64_t post_value = get_lock(&segment.lock);
+            if ((post_value & 1) || (post_value >> 1) > the_transaction->rv || (post_value >> 1) != (prev_value >> 1)){
+                // handle abort logic -> nothing
+                abort_transaction();
+                return false;
+            }
+        }
+    }
+    return false;
 }
 
-bool tm_free(shared_t unused(shared), tx_t unused(tx),
-             void *unused(segment)) noexcept {
-  return true;
+/** [thread-safe] Write operation in the given transaction, source in a private region and target in the shared region.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to use
+ * @param source Source start address (in a private region)
+ * @param size   Length to copy (in bytes), must be a positive multiple of the alignment
+ * @param target Target start address (in the shared region)
+ * @return Whether the whole transaction can continue
+**/
+bool tm_write(shared_t unused(shared), tx_t unused(tx), void const* source, size_t unused(size), void* target) noexcept{
+    struct Region *region = (struct Region *)shared;
+    for (size_t i = 0; i < size / region->align; i++) {
+        uintptr_t the_target = ((uintptr_t)target + region->align * i); 
+        void* the_source = reinterpret_cast<void*>((uintptr_t)source + region->align * i);
+        void* source_copy = malloc(region->align); // be sure to free this
+        memcpy(source_copy, the_source, region->align);
+        the_transaction->write_set.insert({the_target, source_copy});
+    }
+    return true;
+}
+
+/** [thread-safe] Memory allocation in the given transaction.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to use
+ * @param size   Allocation requested size (in bytes), must be a positive multiple of the alignment
+ * @param target Pointer in private memory receiving the address of the first byte of the newly allocated, aligned segment
+ * @return Whether the whole transaction can continue (success/nomem), or not (abort_alloc)
+**/
+Alloc tm_alloc(shared_t shared, tx_t unused(tx), size_t unused(size), void** target) noexcept{
+    Region* region = (Region*) shared;
+    *target = (void *)(region->segment_count.fetch_add(1) << 48ULL);
+    return Alloc::success;
+}
+
+/** [thread-safe] Memory freeing in the given transaction.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to use
+ * @param target Address of the first byte of the previously allocated segment to deallocate
+ * @return Whether the whole transaction can continue
+**/
+bool tm_free(shared_t unused(shared), tx_t unused(tx), void* unused(segment)) noexcept{
+    return true;
 }
